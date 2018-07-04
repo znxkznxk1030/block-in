9296d9825e68395db64bcca79a976ce051867ff8
CryptoSalamander <31682961+CryptoSalamander@users.noreply.github.com>
2018-07-04 21:30:33 +0900

    Chage numHomes 1 again
    ...

--- solidity/hdac.sol
+++ solidity/hdac.sol
@@ -1,13 +1,13 @@
 /// This is made for hdac Hackerton in 2018.
 /// Made by Team Block In
-/// Ver 1.6
+/// Ver 1.5
 pragma solidity ^0.4.0;
 
 import "./permission.sol";
 
 contract hdac is Permission {
     function constuctor() public {
-        numHomes = 0;
+        numHomes = 1;
         balance = 0;
         contractOwner = msg.sender;
     }
